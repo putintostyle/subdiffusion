a2f7f5d25d4953e62c1da4815c4dc9b7b46b220c
putintostyle <jackie84061258@gmail.com>
2022-12-16 23:13:39 +0800

    optimize code

--- subdiffu_main.m
+++ subdiffu_main.m
@@ -14,7 +14,7 @@ function [alpha,result, max_result, sol] = subdiffu_main(alpha, order, Domain_si
     %%%%%
     % order = 2;
     % %%%%%
-    % ref = subdiffusion(order, Nx, D, init, alpha, T, dt,1);
+%     ref = subdiffusion(order, Nx, D, init, alpha, T, dt,1);
     % 
     % x_grid = dx:dx:Domain_size;
     % %%%%%
@@ -25,7 +25,7 @@ function [alpha,result, max_result, sol] = subdiffu_main(alpha, order, Domain_si
     % %%%%%
     
     power = pow_min:pow_max;
-    T_list = T./(2.^power)/10000;
+    T_list = T./(2.^power);
     max_result = [];
     result = [];
     for time_s = length(T_list):-1:1
@@ -34,10 +34,10 @@ function [alpha,result, max_result, sol] = subdiffu_main(alpha, order, Domain_si
         if time_s<length(T_list)
             disp(size(sol_ref(:,1:2:end)))
             disp(size(sol))
-            e = sum((sol(:,2:end)-sol_ref(:,1:2:end)).^2, 1).^(1/2);
+            e = sum(dx.*(sol-sol_ref(:,1:2:end)).^2, 1).^(1/2);
             max_result = [max_result, max(e)];
         end
-        sol_ref = sol(:,2:end);
+        sol_ref = sol;
         result = [result, sol(:,end)];
     end
     % surf(reshape(ref, [Nx, Nx]))
